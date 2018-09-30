25912cd5f90234df458ecb660f751333550e38e4
Matt Hamlin <hamlim@outlook.com>
2018-09-30 19:10:20 -0400

    Babel configuration for the monorepo
    Moved to a shared global babel config and having each package
    extend the shared config.

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _core = _interopRequireDefault(require("@native-bundler/core"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
