ccee9ef6bf959e2ee13402fec8a901ec7cccbe1c
Matt Hamlin <hamlim@outlook.com>
2018-11-21 06:25:04 -0500

    Add error message if the required module does not exist

--- packages/core/src/bundler.js
+++ packages/core/src/bundler.js
@@ -43,7 +43,11 @@ ${JSON.stringify(mod.mapping)}
 
   const result = `(function(modules){
 function require(id) {
-  const [fn, mapping] = modules[id];
+  const mod = modules[id];
+  if (typeof mod === 'undefined') {
+    throw new Error('Attempted to import module that does not exist. Ensure peer dependencies are correctly imported.');
+  }
+  const [fn, mapping] = mod;
   function localRequire(name) {
     return require(mapping[name])
   }
